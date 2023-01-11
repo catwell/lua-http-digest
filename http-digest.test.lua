27f949b4c1a46696e575a8dd04de2fda01a926da
Pierre Chapuis <git@catwell.info>
2023-01-11 11:48:08 +0100

    add more tests, including unauthenticated requests (#11)

--- http-digest.test.lua
+++ http-digest.test.lua
@@ -10,11 +10,6 @@ do -- Find a JSON parser
     assert(ok and json_decode, "no JSON parser found :(")
 end
 
-local T = cwtest.new()
-
-local b, c, _
-
-
 -- To use a local httpbin-go build, use `localhost:8080`.
 local httpbin_domain = "httpbingo.org"
 
@@ -22,39 +17,60 @@ local httpbin_domain = "httpbingo.org"
 local httpbin_authenticated = {authorized = true, user = "user"}
 
 local httpbin_route = httpbin_domain .. "/digest-auth/auth/user/passwd"
-local url = "http://user:passwd@" .. httpbin_route
-local badurl = "http://user:nawak@" .. httpbin_route
+local httpbin_route_no_auth = httpbin_domain .. "/get"
+
+local url = {
+    good_creds = "http://user:passwd@" .. httpbin_route,
+    bad_creds = "http://user:nawak@" .. httpbin_route,
+    no_creds = "http://" .. httpbin_route,
+    good_creds_no_auth = "http://user:passwd@" .. httpbin_route_no_auth,
+    no_creds_no_auth = "http://" .. httpbin_route_no_auth,
+}
+
+local T = cwtest.new()
+local b, c, _
 
 T:start("basics")
 
 -- simple interface
 
-b, c = http_digest.request(url)
+b, c = http_digest.request(url.good_creds)
 T:eq( c, 200 )
 T:eq( json_decode(b), httpbin_authenticated )
 
-_, c = http_digest.request(badurl)
+_, c = http_digest.request(url.bad_creds)
 T:eq( c, 401 )
+
+_, c = http_digest.request(url.no_creds)
+T:eq( c, 401 )
+
+b, c = http_digest.request(url.good_creds_no_auth)
+T:eq( c, 200 )
+T:eq( json_decode(b)["url"], url.no_creds_no_auth )
+
+b, c = http_digest.request(url.no_creds_no_auth)
+T:eq( c, 200 )
+T:eq( json_decode(b)["url"], url.no_creds_no_auth )
 
 -- generic interface
 
 b = {}
 _, c = http_digest.request {
-    url = url,
+    url = url.good_creds,
     sink = ltn12.sink.table(b),
 }
 T:eq( c, 200 )
 b = table.concat(b)
 T:eq( json_decode(b), httpbin_authenticated )
 
-_, c = http_digest.request({url = badurl})
+_, c = http_digest.request({url = url.bad_creds})
 T:eq( c, 401 )
 
 -- with ltn12 source
 
 b = {}
 _, c = http_digest.request {
-    url = url,
+    url = url.good_creds,
     sink = ltn12.sink.table(b),
     source = ltn12.source.string("test"),
     headers = {["content-length"] = 4}, -- 0 would work too
